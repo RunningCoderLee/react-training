8b18fe849ce0b37df13040ea57cc652afcee9f34
Kevin Lee <kevin@KevindeMac-mini.local>
2017-01-15 22:40:59 +0800

    手动链接redux

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,6 +5,33 @@ import About from 'Components/test/About';
 import Repos from 'Components/test/Repos';
 import Repo from 'Components/test/Repo';
 import Home from 'Components/test/Home';
+import { createStore } from 'redux';
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+let currentVal = store.getState();
+
+const listener = () => {
+  const previousVal = currentVal;
+  currentVal = store.getState();
+  console.log('pre state: ', previousVal, 'next state: ', currentVal);
+};
+
+store.subscribe(listener);
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
 
 const routes = (
   <Router history={browserHistory}>
